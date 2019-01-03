b1355f635c06d275fbdc45c0dd7fdd1fe87b1fe2
Unperknown <wnsdud105@gmail.com>
2019-01-03 10:30:44 +0900

    #unperknown-5 [ADDED] two results show in one window

--- src/AINOMOK/Image_Processing/detect.py
+++ src/AINOMOK/Image_Processing/detect.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-sensitivity = 85
+sensitivity = 70
 ESC = 27
-lowerWhite = np.array([0,0,195])
-upperWhite = np.array([255,75,255])
+lowerWhite = np.array([0,0,255 - sensitivity])
+upperWhite = np.array([255,sensitivity,255])
 
 lowerBlack = np.array([0,0,0])
-upperBlack = np.array([360,360,60])
+upperBlack = np.array([360,360,115])
 
 if __name__ == '__main__':
 	capture = cv2.VideoCapture(1)
@@ -19,8 +19,9 @@ if __name__ == '__main__':
 		maskB = cv2.inRange(hsv, lowerBlack, upperBlack)
 		resultB = cv2.bitwise_and(hsv, hsv, mask=maskB)
 		cv2.imshow('Before', frame)
-		cv2.imshow('White', resultW)
-		cv2.imshow('Black', resultB)
+		np.hstack((resultW, resultB))
+		result = np.concatenate((resultW, resultB), axis=1)
+		cv2.imshow('Result', result)
 		c = cv2.waitKey(10)
 		if c == ESC:
 			break
